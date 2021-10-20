dcc1b344e006c10d858048f0665d437d8463edd4
ibrahimhansu <57485063+ibrahimhansu@users.noreply.github.com>
2021-10-21 00:55:57 +0300

    Add files via upload

---
+++ week4/Products.java
@@ -0,0 +1,49 @@
+//   w/cansu çelik
+
+
+package week4;
+
+import java.util.*;
+public class Products  {
+    String name;
+    String desc;
+    double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscountedPrice() {
+        return discountedPrice;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    double discountedPrice;
+    String category;
+    String subCategory;
+
+    Products (String name, String desc, double price, double discountedPrice, String category, String subCategory){
+        this.name=name;
+        this.desc=desc;
+        this.price=price;
+        this.discountedPrice=discountedPrice;
+        this.category=category;
+        this.subCategory=subCategory;
+    }
+
+}
